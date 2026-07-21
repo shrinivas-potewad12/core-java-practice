96ce067b211e5501b2870805057963e31f7b0f8d
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-21 23:29:11 +0530

    Update ClassA.java

---
+++ Day01/ClassA.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class ClassA {
+	
+	void meth1(int x, int y,int z)
+	{
+	 System.out.println("Addition : "+(x+y+z));
+	 new ClassA().meth2(100,99);
+	}
+	void meth2(int a,int b)
+	{
+		System.out.println("substraction :"+(a-b));
+	}
+	ClassA()
+	{
+		System.out.println("division :"+(10/2));
+	}
+	ClassA(int x)
+	{
+		System.out.println("multiplication : "+(x*50));
+	}
+	 public static void main(String[] args)
+	 {
+		 new ClassA(10).meth1(5, 10, 15);
+    	   
+    	
+	 }
