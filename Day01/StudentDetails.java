09374d5c2f4accfdba74ce25177da6ba9c6b769e
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-23 22:57:05 +0530

    Add StudentDetails class with student information

---
+++ Day01/StudentDetails.java
@@ -0,0 +1,19 @@
+public class StudentDetails {
+
+    public static void main(String[] args) {
+
+        int rollNo = 101;
+        String name = "Shrinivas";
+        char grade = 'A';
+        float marks = 89.5f;
+        double percentage = 89.50;
+        boolean pass = true;
+
+        System.out.println(rollNo);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(marks);
+        System.out.println(percentage);
+        System.out.println(pass);
+    }
+}
