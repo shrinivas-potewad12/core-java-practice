3bc4503c8261a17fa2b3a11aabcfa50298c1c9b3
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-23 22:55:02 +0530

    Add Employee class with main method

---
+++ Day01/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+
+    public static void main(String[] args) {
+
+        String empName = "Rahul";
+        double salary = 35000.75;
+
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Salary: " + salary);
+    }
+}
