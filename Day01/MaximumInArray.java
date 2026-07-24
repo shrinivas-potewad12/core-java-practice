65256c3fd1522caf68a073345475edb777761606
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-24 23:17:19 +0530

    Add MaximumInArray class to find max in array

---
+++ Day01/MaximumInArray.java
@@ -0,0 +1,14 @@
+public class MaximumInArray {
+    public static void main(String[] args) {
+        int[] arr = {15, 8, 50, 22};
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        System.out.println(max);
+    }
+}
