b2baa4c81239c6616b9e296ba8b99480609a4964
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-25 23:12:49 +0530

    Fix formatting issues in ReverseNumber.java

--- Day01/ReverseNumber.java
+++ Day01/ReverseNumber.java
@@ -2,7 +2,6 @@ public class ReverseNumber {
     public static void main(String[] args) {
         int n = 1234;
         int rev = 0;
-
         while (n > 0) {
             rev = rev * 10 + n % 10;
             n /= 10;
