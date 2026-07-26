99e1b46e3c135b4d29d46677ed81aa980564c5b8
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-26 20:48:06 +0530

    Update CountDigits.java

--- Day01/CountDigits.java
+++ Day01/CountDigits.java
@@ -2,7 +2,6 @@ public class CountDigits {
     public static void main(String[] args) {
         int n = 12345;
         int count = 0;
-
         while (n > 0) {
             count++;
             n /= 10;
