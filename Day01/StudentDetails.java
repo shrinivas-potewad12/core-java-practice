5d7e99678d10a9e932745cf3d6a9130dad466510
Shrinivas potewad <157982107+shrinivas-potewad12@users.noreply.github.com>
2026-07-30 23:03:44 +0530

    Update StudentDetails.java

--- Day01/StudentDetails.java
+++ Day01/StudentDetails.java
@@ -2,7 +2,9 @@ public class StudentDetails {
 
     public static void main(String[] args) {
         int rollNo = 101;
+
         String name = "shinu";
+
         char grade = 'A';
         float marks = 89.5f;
         double percentage = 89.50;
